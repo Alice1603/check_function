12f8388dc5ce16176623d8c0c983af0e99cfd750
Alice1603 <58571562+Alice1603@users.noreply.github.com>
2020-10-22 14:58:43 +0300

    Add files via upload

--- date_autumn.py
+++ date_autumn.py
@@ -4,7 +4,7 @@ def date_autumn(dates):
     g = []
     for i in range(len(a)):
         s = a[i].split("-")
-        if s[1] in k:
+        if s[0] in k:
             g.append(a[i])
     n = g[0]
     for i in range(len(g)):
@@ -13,10 +13,10 @@ def date_autumn(dates):
         if int(s[2]) > int(n[2]):
             n = g[i]
         elif int(s[2]) == int(n[2]):
-            if int(s[1]) > int(n[1]):
+            if int(s[0]) > int(n[0]):
                 n = g[i]
-            elif int(s[1]) == int(n[1]):
-                if int(s[0]) > int(n[0]):
+            elif int(s[0]) == int(n[0]):
+                if int(s[1]) > int(n[1]):
                     n = g[i]
                 else:
                     n = "-".join(n)
@@ -25,3 +25,7 @@ def date_autumn(dates):
         else:
             n = "-".join(n)
     return n
+
+
+dates = {"11-27-2006","12-01-2009","08-31-2010","11-28-2008"}
+print(date_autumn(dates))
