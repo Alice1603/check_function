63498e444918898fd004f99b753ac45d45a945de
Alice1603 <58571562+Alice1603@users.noreply.github.com>
2020-10-27 22:47:40 +0300

    Delete remove_palindrom.py

--- remove_palindrom.py
+++
@@ -1,9 +0,0 @@
-def remove_palindroms(spells):
-    global new_spells
-    s = [i for i in spells]
-    g = []
-    for i in range(len(s)):
-        k = 0
-        a = ("".join(s[i].split())).lower()
-        if a == a[::-1]:
-            new_spells.remove(s[i])
