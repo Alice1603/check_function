ba88b355c64746a6179df9d2eca667d83777f2ca
Alice1603 <58571562+Alice1603@users.noreply.github.com>
2020-10-30 21:48:40 +0300

    Add files via upload

--- remove_palindroms.py
+++ remove_palindroms.py
@@ -2,6 +2,5 @@ def remove_palindroms(spells):
     s = [i for i in spells]
     for i in range(len(s)):
         a = ("".join(s[i].split())).lower()
-        if a == a[::-1] and s[i] in new_list:
+        if a == a[::-1] and (s[i] in spells):
             spells.remove(s[i])
-    return spells
